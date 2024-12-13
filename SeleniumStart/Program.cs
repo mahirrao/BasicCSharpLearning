9f3955316161cecb143248f8f2cebcc02066ac95
mahirrao <md.ahirrao@gmail.com>
2024-12-13 18:29:08 +0530

    Add project files.

---
+++ SeleniumStart/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+using OpenQA.Selenium.Chrome;
+
+Console.WriteLine("Hello, Selenium!");
+
+
+ChromeDriver driver = new ChromeDriver();
+driver.Url = "http://way2automation.com";
+Console.WriteLine(driver.Title);
+Debug.WriteLine(driver.Title);
+driver.Close();
+driver.Quit();
